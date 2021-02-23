5100e1e6fca8d17749dd734b746682c66a47e194
Tanner Muro <tmuro17@gmail.com>
2021-02-22 21:02:19 -0500

    Fixed start.sh

--- start.sh
+++ start.sh
@@ -5,9 +5,9 @@ export PORT=4792
 
 echo "Stopping old copy..."
 
-/home/hw06/hw06/bulls-and-cows/_build/prod/rel/bulls/bin/bulls stop || true
+/home/hw06/hw06/_build/prod/rel/bulls/bin/bulls stop || true
 
 echo "Starting app..."
 
 
-/home/hw06/hw06/bulls-and-cows/_build/prod/rel/bulls/bin/bulls start
+/home/hw06/hw06/_build/prod/rel/bulls/bin/bulls start
